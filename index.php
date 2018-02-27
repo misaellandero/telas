fb8905ddd38867b59b80c10195857899ea574ece
misaellandero <misaellandero@landercorp.com.mx>
2018-02-27 14:54:05 -0600

    cambio en el login
    se actulizaraon datos de sesion

--- index.php
+++ index.php
@@ -1,51 +1,154 @@
 <?php require_once('Connections/conexion_usuarios.php'); ?>
 <?php
+
 mysql_select_db($database_conexion_usuarios, $conexion_usuarios);
+
 $query_consulta_usuarios = "SELECT * FROM usuarios";
+
 $consulta_usuarios = mysql_query($query_consulta_usuarios, $conexion_usuarios) or die(mysql_error());
+
 $row_consulta_usuarios = mysql_fetch_assoc($consulta_usuarios);
+
 $totalRows_consulta_usuarios = mysql_num_rows($consulta_usuarios);
+
 ?><?php
+
 // *** Validate request to login to this site.
+
 if (!isset($_SESSION)) {
+
   session_start();
+
 }
+
+
 
 $loginFormAction = $_SERVER['PHP_SELF'];
+
 if (isset($_GET['accesscheck'])) {
+
   $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+
 }
+
+
 
 if (isset($_POST['usuario'])) {
+
   $loginUsername=$_POST['usuario'];
+
   $password=$_POST['contrasena'];
+
   $MM_fldUserAuthorization = "";
+
   $MM_redirectLoginSuccess = "entrar.php";
+
   $MM_redirectLoginFailed = "index.php";
+
   $MM_redirecttoReferrer = false;
+
   mysql_select_db($database_conexion_usuarios, $conexion_usuarios);
 
-  $LoginRS__query=sprintf("SELECT user, password FROM usuarios WHERE user='%s' AND password='%s'",
+
+
+  $LoginRS__query=sprintf("SELECT user, password ,id_empleado FROM usuarios WHERE user='%s' AND password='%s'",
+
     get_magic_quotes_gpc() ? $loginUsername : addslashes($loginUsername), get_magic_quotes_gpc() ? $password : addslashes($password));
 
+
+
   $LoginRS = mysql_query($LoginRS__query, $conexion_usuarios) or die(mysql_error());
+
   $loginFoundUser = mysql_num_rows($LoginRS);
+
   if ($loginFoundUser) {
-     $loginStrGroup = "";
+
+   $row=mysql_fetch_array($LoginRS);
+
+    $loginStrGroup = "";
+
+
+
+$sql = "SELECT *, (SELECT nombre FROM modulos where id = permisos_modulo.modulo) as modulo FROM  permisos_modulo WHERE id_usuario = {$row['id_empleado']}  AND permiso = '1' ";
+
+$res = mysql_query($sql);
+
+$modulos = array();
+
+$pestanas = array();
+
+
+
+
+
+
+
+
+
+$var_id = $row['id_empleado'];
+
+
+
+while ($row=mysql_fetch_array($res)) {
+
+
+
+$sql = "SELECT *,(SELECT nombre FROM modulos where id = p.id_de_modulo) as nombre_modulo FROM permiso_pestanas as p WHERE id_modulo = {$row['id']}  AND permiso = '1' ";
+
+
+
+$res_pestanas = mysql_query($sql);
+
+
+
+  while ($row_pestanas = mysql_fetch_array($res_pestanas)) {$pestanas[] = array( $row_pestanas['nombre_modulo'] => $row_pestanas['pestana']);}
+
+
+
+
+
+
+
+  $modulos[] = $row['modulo'];}
+
+
 
     //declare two session variables and assign them
+
     $_SESSION['MM_Username'] = $loginUsername;
+
     $_SESSION['MM_UserGroup'] = $loginStrGroup;
 
+    $_SESSION['MM_UserId'] = $var_id;
+
+    $_SESSION['MM_Modulos'] = $modulos;
+
+    $_SESSION['MM_pestanas'] = $pestanas;
+
+
+
+
+
+
+
     if (isset($_SESSION['PrevUrl']) && false) {
+
       $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];
+
     }
+
     header("Location: " . $MM_redirectLoginSuccess );
+
   }
+
   else {
+
     header("Location: ". $MM_redirectLoginFailed );
+
   }
+
 }
+
 ?><!DOCTYPE html>
 
 
