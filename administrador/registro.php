6ddcc248b7c0a946beeb1f00eb65c07932b3935b
misaellandero <misaellandero@landercorp.com.mx>
2018-02-27 12:15:38 -0600

    se remplazo todo
    se remplazo todo con los archivos originales ☹️

--- administrador/registro.php
+++ administrador/registro.php
@@ -1,23 +1,7 @@
 <?php
 
 require_once('../funciones.php');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-conectar('localhost','root','CornComputerInc1*','sistema');
-=======
 conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
 
 //Recibir
 $Id = strip_tags($_POST['Id']);
@@ -26,64 +10,16 @@ $descripsion = strip_tags($_POST['descripsion']);
 $query = @mysql_query('SELECT * FROM modelo  WHERE Id="'.mysql_real_escape_string($Id).'"');
 if($existe = @mysql_fetch_object($query))
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	echo "<script type=\"text/javascript\">alert('Este Modelo ya esta rgistrado, Use otra clave o use la funcion Editar datos'); window.location='entrar.php';</script>";
-
-
-=======
 	echo "<script type=\"text/javascript\">alert('Este Modelo ya esta rgistrado, Use otra clave o use la funcion Editar datos'); window.location='entrar.php';</script>"; 
 	
 	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Este Modelo ya esta rgistrado, Use otra clave o use la funcion Editar datos'); window.location='entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Este Modelo ya esta rgistrado, Use otra clave o use la funcion Editar datos'); window.location='entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Este Modelo ya esta rgistrado, Use otra clave o use la funcion Editar datos'); window.location='entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
 }else{
 	$meter = @mysql_query('INSERT INTO modelo (Id, descripsion) values ("'.$Id.'", "'.mysql_real_escape_string($descripsion).'",');
 	if($meter)
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>";
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro'); window.location='../entrar.php';</script>";
-=======
 	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
 	}else{
 		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
 	}
 }
 
--- administrador/registrote.php
+++ administrador/registrote.php
@@ -1,23 +1,7 @@
 <?php
 
 require_once('../funciones.php');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-conectar('localhost','root','CornComputerInc1*','sistema');
-=======
 conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-conectar('localhost', 'u722193362_root','03032014','u722193362_date');
->>>>>>> parent of fd9289d... Revert "commit inicial"
 
 //Recibir
 $Id = strip_tags($_POST['Id']);
@@ -28,64 +12,16 @@ $descripsion = strip_tags($_POST['descripsion']);
 $query = @mysql_query('SELECT * FROM tela  WHERE Id ="'.mysql_real_escape_string($Id).'"');
 if($existe = @mysql_fetch_object($query))
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	echo "<script type=\"text/javascript\">alert('Esta tela ya esta registrado, Use otra clave o use la funcion Editar datos'); window.location='../entrar.php';</script>";
-
-
-=======
 	echo "<script type=\"text/javascript\">alert('Esta tela ya esta registrado, Use otra clave o use la funcion Editar datos'); window.location='../entrar.php';</script>"; 
 	
 	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Esta tela ya esta registrado, Use otra clave o use la funcion Editar datos'); window.location='../entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Esta tela ya esta registrado, Use otra clave o use la funcion Editar datos'); window.location='../entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo "<script type=\"text/javascript\">alert('Esta tela ya esta registrado, Use otra clave o use la funcion Editar datos'); window.location='../entrar.php';</script>"; 
-	
-	
->>>>>>> parent of fd9289d... Revert "commit inicial"
 }else{
 	$meter = @mysql_query('INSERT INTO tela (Id, Descripcion) values ("'.$Id.'", "'.mysql_real_escape_string($descripsion).'")');
 	if($meter)
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>";
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro XDP'); window.location='../entrar.php';</script>";
-=======
 	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
 	}else{
 		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro XDP'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro XDP'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro XDP'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
-=======
-	echo"<script type=\"text/javascript\">alert('El articulo ha sido registrado con exito'); window.location='../entrar.php';</script>"; 
-	}else{
-		echo"<script type=\"text/javascript\">alert('Hubo un error en el registro XDP'); window.location='../entrar.php';</script>"; 
->>>>>>> parent of fd9289d... Revert "commit inicial"
 	}
 }
 
